b0a42e16bdf0ca1865563f108f87df2cca6061fd
boriss2808 <boris.simlesa@gmail.com>
2026-04-19 21:44:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'barijera-v1';
+const CACHE = 'barijera-v2';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
 self.addEventListener('fetch', e => {
