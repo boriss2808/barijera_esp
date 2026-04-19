a9e5337e67df0795338c6a70e7ec13e82dd155d5
boriss2808 <boris.simlesa@gmail.com>
2026-04-19 21:36:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-const CACHE='barijera-pwa-v1';
-self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
-self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request))));
+const CACHE = 'barijera-v1';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
